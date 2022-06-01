54bcda4495035030b7c7cb7b59384b29f4ed9f8e
Edward Colmenarez <edward.colmenarez@betaboxtechnologies.com>
2022-06-01 15:27:57 -0400

    id y secret añadidos

--- public/html-template/app/src/components/ModalAPlicaciones.jsx
+++ public/html-template/app/src/components/ModalAPlicaciones.jsx
@@ -1,13 +1,19 @@
-import React, { useRef, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Formik, Form, Field, FieldArray,ErrorMessage } from 'formik'
+import React, { useEffect, useRef, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Formik, Form, Field, FieldArray, ErrorMessage, useFormikContext } from 'formik'
 
 import { AiOutlinePlus, AiFillEye } from 'react-icons/ai'
 import { MdOutlineFileCopy } from 'react-icons/md'
 
-    
+const CustomInput = () => {
+    return (
+    <>
+        <input/>
+    </>
+    )
+}
 
-export const ModalAPlicaciones = ({view, hide, add,id = "189da1-gfsaf-1561f-gfh1561"}) => {
+export const ModalAPlicaciones = ({view, hide, add }) => {
 
     const appurls = [
         {
@@ -15,24 +21,54 @@ export const ModalAPlicaciones = ({view, hide, add,id = "189da1-gfsaf-1561f-gfh1
         }
     ]
 
+    const formikRef = useRef()
+
+    const [ type, setInputType] = useState('password')
+    const [ clientId, setCLientId] = useState('')
+
+    const CLIENT_ID_URL = 'https://auth.synapse-crm.com/api/generator/client-id'
+    const SECRET_URL = 'https://auth.synapse-crm.com/api/generator/secret'
+
+    useEffect(()=> {
+        fetch(SECRET_URL)
+                .then( res => res.json() )
+                .then( res => {
+                    if (formikRef.current) {
+                        formikRef.current.setFieldValue(
+                          "secret",
+                          res.data
+                        )
+                    }
+                    // 
+                } )
+                .catch( err => console.log(err))
+        fetch(CLIENT_ID_URL)
+                .then( res => res.json() )
+                .then( res => setCLientId(res.data) )
+                .catch( err => console.log(err) )
+        
+    }, [])
+
+    const changeType = () => {
+        type === 'password'? setInputType('text'): setInputType('password') 
+    }
+
   return (
     <div style={view ?{display: 'flex'}:{display: 'none'}} className='justify-end bg-black-transparent z-50 absolute top-0 bottom-0 left-0 right-0 m-[0 auto]' >
         <div className='bg-[#ffffff] dark:bg-darkmode-black-02 w-[40rem] h-full px-16 py-12' >
             <h2 className='dark:text-[#ffffff] text-[2rem] font-semibold' >Crear una aplicacion</h2>
-            <div className='flex' >
-                <div className='p-4 bg-pink rounded-full mr-3' >
-                    <AiOutlinePlus className=' text-white text-[3rem] ' />
-                </div>
-                <div>
-                    <input className='dark:focus-visible:outline-none dark:bg-darkmode-black-02 dark:text-[#ffffff] focus-visible:outline-none mb-[.4rem] border-b border-b-gray-border' type="text" placeholder='Nombre de la app' />
-                    <p className='dark:text-[#ffffff]' >Client ID</p>
-                    <p className='text-dark-blue dark:text-darkmode-blue-01 font-medium' >{id}</p>
-                </div>
-            </div>
+            
             <Formik
-            initialValues={{ fabricante: '', website: '', appurls }}
+            innerRef={formikRef}
+            initialValues={{ fabricante: '', website: '', secret: '' , appurls, applicationName: '' }}
+            onSubmit={ (values) => {
+                console.log(values.appurls[0].appurl.length)
+            }}
              validate={values => {
                     const errors = {};
+                    if (!values.applicationName) {
+                        errors.applicationName = 'El Nombre de la aplicacion es obligatorio';
+                    }
                     if (!values.fabricante) {
                     errors.fabricante = 'El fabricante es obligatorio';
                     }
@@ -42,10 +78,27 @@ export const ModalAPlicaciones = ({view, hide, add,id = "189da1-gfsaf-1561f-gfh1
                     if (!values.website) {
                     errors.website = 'El website es obligatorio';
                     }
+                    if (!values.secret){
+                        errors.secret = 'Por favor vuelva a generar el secret'
+                    }
+                    if(values.appurls[0].appurl.length < 2){
+                        errors.appurls = 'Debe colocar almenos una url a la aplicacion'
+                    }
                     return errors;
                 }} >
-                {({values}) => (
+                {({values, ...props}) => (
+                    
                     <Form className='py-12' >
+                        <div className=' mb-7 flex' >
+                            <div className='p-4 bg-pink rounded-full mr-3' >
+                                <AiOutlinePlus className=' text-white text-[3rem] ' />
+                            </div>
+                            <div>
+                                <Field name='applicationName' className='dark:focus-visible:outline-none dark:bg-darkmode-black-02 dark:text-[#ffffff] focus-visible:outline-none mb-[.4rem] border-b border-b-gray-border' type="text" placeholder='Nombre de la app' />
+                                <p className='dark:text-[#ffffff]' >Client ID</p>
+                                <p className='text-dark-blue dark:text-darkmode-blue-01 font-medium' >{clientId}</p>
+                            </div>
+                        </div>
                     <div className='flex flex-col w-full' >
                         <label className='mb-2 text-dark-blue dark:text-darkmode-blue-01 font-normal' htmlFor="fabricante" >Fabricante</label>
                         <Field name='fabricante' className='dark:focus-visible:outline-none focus-visible:outline-none dark:bg-darkmode-black-02 dark:text-[#ffffff] rounded px-3 py-2 focus:dark:bg-darkmode-black-02  mb-8 border-2 border-gray-light' id='fabricante' type="text" />
@@ -57,12 +110,20 @@ export const ModalAPlicaciones = ({view, hide, add,id = "189da1-gfsaf-1561f-gfh1
                     <div className='relative flex flex-col w-full' >
                         <div className='flex justify-between' >
                             <label className='mb-2 text-dark-blue dark:text-darkmode-blue-01 font-normal' htmlFor="secret" >Secret</label>
-                            <button className='text-pink' >Regenerar</button>
+                            <button type='button' className='text-pink' onClick={() =>{ 
+                                props.setFieldValue('secret', '')
+                                fetch(SECRET_URL)
+                                .then( res => res.json() )
+                                .then( res => props.setFieldValue('secret', res.data) )
+                                .catch( err => console.log(err) )
+                                }} >Regenerar</button>
                         </div>
-                        <Field name='secret' className='dark:focus-visible:outline-none focus-visible:outline-none dark:bg-darkmode-black-02 dark:text-[#ffffff] rounded px-3 py-2 focus:dark:bg-darkmode-black-02  mb-8 border-2 border-gray-light' id='secret' type="password" />
+                        <Field disabled  name='secret' className='dark:focus-visible:outline-none focus-visible:outline-none dark:bg-darkmode-black-02 dark:text-[#ffffff] rounded px-3 py-2 focus:dark:bg-darkmode-black-02  mb-8 border-2 border-gray-light' id='secret' type={type} />
                         <div className='w-12 flex justify-between absolute right-[1.25rem] bottom-[2.75rem]' >
-                            <button className='text-gray-light text-[1.2rem]' ><MdOutlineFileCopy/></button>
-                            <button className='text-gray-light text-[1.2rem]' ><AiFillEye/></button>
+                            <button onClick={async() => {
+                               await navigator.clipboard.writeText(values.secret)
+                            }} type="button" className='text-gray-light text-[1.2rem]' ><MdOutlineFileCopy/></button>
+                            <button onClick={changeType} type="button" className='text-gray-light text-[1.2rem]' ><AiFillEye/></button>
                         </div>
                     </div>
                     <div className='flex flex-col w-full' >
@@ -71,11 +132,11 @@ export const ModalAPlicaciones = ({view, hide, add,id = "189da1-gfsaf-1561f-gfh1
                         {({ insert, remove, push }) => (
                         <div className='overflow-scroll overflow-x-hidden max-h-[11rem] scrollbar-thin scrollbar-thumb-pink px-4 scrollbar-track-black-transparent dark:scrollbar-track-gray-border ' >
                             {values.appurls.length > 0 &&
-                            values.appurls.map((appurl, index) => (
+                            values.appurls.map((appurls, index) => (
                                 <div className="mb-2" key={index}>
                                 <div className="flex flex-col w-full">
                                     <Field
-                                    name={`appurl.${index}.email`}
+                                    name={`appurls.${index}.appurl`}
                                     type="text"
                                     className='dark:focus-visible:outline-none focus-visible:outline-none dark:bg-darkmode-black-02 dark:text-[#ffffff] rounded px-3 py-2 focus:dark:bg-darkmode-black-02  border-2 border-gray-light'
                                     />
@@ -93,16 +154,19 @@ export const ModalAPlicaciones = ({view, hide, add,id = "189da1-gfsaf-1561f-gfh1
                         )}
                     </FieldArray>
                     </div>
+                    <ErrorMessage className='text-red' name='applicationName' component='p' />
                     <ErrorMessage className='text-red' name='fabricante' component='p' />
                     <ErrorMessage className='text-red' name='website' component='p' />
-                    <ErrorMessage className='text-red' name='appurl' component='p' />
+                    <ErrorMessage className='text-red' name='secret' component='p' />
+                    <ErrorMessage className='text-red' name='appurls' component='p' />
+                    <div className='mt-4 flex justify-between' >
+                        <button className='w-[45%] bg-[#ffffff] border border-solid rounded-[10px] font-semibold px-3 py-2 dark:bg-darkmode-black-02 dark:text-[#ffffff] dark:border-pink ' onClick={hide} >Volver</button>
+                        <button type='submit' className='w-[45%] bg-pink text-white rounded-[10px] font-semibold px-3 py-2'  >Guardar</button>
+                    </div>
                 </Form>
                 ) }
             </Formik>
-        <div className='flex justify-between' >
-            <button className='w-[45%] bg-[#ffffff] border border-solid rounded-[10px] font-semibold px-3 py-2 dark:bg-darkmode-black-02 dark:text-[#ffffff] dark:border-pink ' onClick={hide} >Volver</button>
-            <button className='w-[45%] bg-pink text-white rounded-[10px] font-semibold px-3 py-2' onClick={add} >Guardar</button>
-        </div>
+        
         </div>
     </div>
   )
