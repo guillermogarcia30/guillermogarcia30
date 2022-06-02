5805a0ba5abb96134897accc9584137184aa1fdb
Edward Colmenarez <edward.colmenarez@betaboxtechnologies.com>
2022-06-02 15:17:44 -0400

    commit

--- public/html-template/app/src/App.jsx
+++ public/html-template/app/src/App.jsx
@@ -25,7 +25,16 @@ function App() {
   useEffect(function(){
     appTheme();
     getUser().then( res => {
-      dispatch(addApp(res))
+      res.map( el => {
+        dispatch(addApp({
+          logo: el.image || '',
+          title: el.name, 
+          token: el.id,
+          secret: el.secret, 
+          status: el.status === 0 ? true : false, 
+          id: el.token
+         }))
+      } ) 
     })
     .catch( console.log('algo salió mal'))
     
