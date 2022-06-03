73db1d56b0a4a6ee88b8efaad86c931c2f2e6def
Guillermo García <guillermo.garcia@betaboxtechnologies.com>
2022-06-03 14:35:02 -0400

    APIS de paises

---
+++ app/Http/Controllers/API/CountryController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Country;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        set_time_limit(0);
+        header('Content-Type: application/json; charset=utf-8');
+        header('Accept: application/json');
+
+        $country_count = Country::count();
+        if($country_count == 0){
+            return response([
+                'error' => true,
+                'description' => 'Not Found Resource'
+            ],404);
+        }else{
+            $country = Country::orderBy('country_name','ASC')->get();
+            return response([
+                'error' => false,
+                'data' => $country
+            ],200);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        set_time_limit(0);
+        header('Content-Type: application/json; charset=utf-8');
+        header('Accept: application/json');
+
+        $country_count = Country::where('id','=',$id)->count();
+        if($country_count == 0){
+            return response([
+                'error' => true,
+                'description' => 'Not Found Resource'
+            ],404);
+        }else{
+            $country = Country::where('id','=',$id)->first();
+            return response([
+                'error' => false,
+                'data' => $country
+            ],200);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
