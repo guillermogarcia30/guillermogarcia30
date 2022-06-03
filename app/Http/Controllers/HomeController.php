fdabed0bb0dbb527dc9d1a5ca3630063538fbd88
Guillermo García <guillermo.garcia@betaboxtechnologies.com>
2022-06-03 15:12:26 -0400

    testeando valores de secciones

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        
         $user = auth()->user();
         $tokenResult = $user->createToken('Personal Access Token');
         $token = $tokenResult->token;
