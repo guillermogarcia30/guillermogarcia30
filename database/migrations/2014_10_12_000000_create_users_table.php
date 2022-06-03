26a10eb988307e87e337297ab8e7c35f0fe9781d
Guillermo García <guillermo.garcia@betaboxtechnologies.com>
2022-06-03 13:24:17 -0400

    agregando migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id');
             $table->uuid('tenant_id');
+            $table->uuid('country_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
+            $table->date('birth_date')->nullable();
+            $table->string('address',100)->nullable();
+            $table->string('phone',15)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
