ad5907d35af799e5e38c9d5346d18571b6fcf82e
Guillermo García <guillermo.garcia@betaboxtechnologies.com>
2022-06-03 16:07:03 -0400

    depurando

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -92,6 +92,7 @@ class UserController extends Controller
         $campos = array(
             DB::raw("
                 DISTINCT
+                oauth_clients.id AS id,
                 oauth_clients.name AS name,
                 '' AS extra
             ")
@@ -106,7 +107,6 @@ class UserController extends Controller
 
 
         $extra = [];
-        /*
         foreach ($authorized_apps as $key => $row) {
             $campos_extra = array(
                 DB::raw("
@@ -126,8 +126,7 @@ class UserController extends Controller
                             ->select($campos_extra)
                             ->first();
         }
-        */
-        
+
         $data = [
             'user' => $user,
             'authorized_apps' => $authorized_apps
