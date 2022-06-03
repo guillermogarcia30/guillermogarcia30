53ed1887b8fe7c7c213d1bb4af243ed2f739aeb7
Guillermo García <guillermo.garcia@betaboxtechnologies.com>
2022-06-03 09:44:11 -0400

    nombres

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -92,13 +92,7 @@ class UserController extends Controller
         $campos = array(
             DB::raw("
                 DISTINCT
-                oauth_clients.id AS id,
-                oauth_clients.image AS image,
-                oauth_clients.maker AS maker,
-                oauth_clients.website AS website,
-                oauth_clients.secret AS secret,
-                oauth_clients.redirect AS redirect,
-                oauth_clients.revoked AS status
+                oauth_clients.image AS name
             ")
         );
 
