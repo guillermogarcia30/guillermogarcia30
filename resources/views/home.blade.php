971887d972215d23d5bb01dbb000bc3c436694e5
Keinier Caboverde <keinier.caboverde@betaboxtechnologies.com>
2022-06-06 11:13:39 -0400

    Fusion entre REACT y Laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app2')
 
 @section('content')
+<noscript>You need to enable JavaScript to run this app.</noscript>
 <div id="root"></div>
 @endsection
--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -1,30 +1,20 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    
-    <link href="{{asset('html-template/app/src/index.css')}}" rel="stylesheet">
-
-</head>
-<body>
-    @yield('content')
-
-    <script defer type="module" src="{{asset('html-template/app/src/index.js')}}" ></script>
-</body>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="icon" href="/favicon.ico"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+        <meta name="theme-color" content="#000000"/>
+        <meta name="description" content="Web site created using create-react-app"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,400&display=swap" rel="stylesheet">
+        <link rel="manifest" href="/manifest.json"/>
+        <title>Synapse</title>
+        <script defer="defer" src="/static/js/main.af62e1c4.js"></script>
+        <link href="/static/css/main.869a7136.css" rel="stylesheet">
+    </head>
+    <body>
+        @yield('content')
+    </body>
 </html>
