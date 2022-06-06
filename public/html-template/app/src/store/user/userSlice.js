e1410e7c2bffe847920eac7741d68647b3367c85
Edward Colmenarez <edward.colmenarez@betaboxtechnologies.com>
2022-06-06 16:28:54 -0400

    commit

--- public/html-template/app/src/store/user/userSlice.js
+++ public/html-template/app/src/store/user/userSlice.js
@@ -1,17 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    token: '123456789',
-    email: 'edwardcolmenarez89@gmail.com',
+    token: '',
+    email: '',
     tenant: '',
-    ciudad: 'Maracay',
-    pais: 'Arabia saudita',
-    name: 'Edward Colmenarez',
-    birthDate: '18/09/200',
-    address: 'Caña de azúcar sector 2 vereda 15 casa 98',
-    phone: '+58 424-356-7502',
-    position: 'Programador front-end en Betabox Technologies',
-    image: 'http://vignette2.wikia.nocookie.net/joke-battles/images/0/09/Goku.png/revision/latest?cb=20151230234609'
+    ciudad: '',
+    pais: '',
+    name: '',
+    birthDate: '',
+    address: '',
+    phone: '',
+    position: '',
+    image: ''
 }
 
 const userSlice = createSlice({
@@ -29,7 +29,7 @@ const userSlice = createSlice({
             state.address = action.payload.address? action.payload.address : 'Direccion no especificada'
             state.phone = action.payload.phone? action.payload.phone : ''
             state.position = action.payload.position ? action.payload.position : 'Posicon laboral no especificada'
-            state.image = action.payload.image ? action.payload.image : 'https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png'
+            state.image = action.payload.image ? action.payload.image : 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
             state.ciudad = action.payload.ciudad ? action.payload.ciudad : 'Ciudad no especificada'
             state.pais = action.payload.pais ? action.payload.ciudad : 'Pais no especificado'
         }
