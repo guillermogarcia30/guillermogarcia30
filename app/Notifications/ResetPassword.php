c28ca4845e4c1b9c6e887b3933d512ad2fc7dc94
Guillermo García <guillermo.garcia@betaboxtechnologies.com>
2022-06-07 14:31:00 -0400

    colocar plantilla de correo de Forgot Password en base al prototipo #1554

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -20,10 +20,12 @@ class ResetPassword extends ResetPasswordNotification
         return (new MailMessage)
             ->greeting('Hola '.$notifiable->name)
             ->subject(Lang::get('Solicitud de restablecimiento de contraseña'))
-            ->line(new HtmlString("Se solicitó un restablecimiento de contraseña para tu cuenta <strong style='color:#000000;'>" .$notifiable->getEmailForPasswordReset(). "</strong>, haz clic en el botón que aparece a continuación para cambiar tu contraseña."))
+            ->line(new HtmlString("Estás recibiendo este correo electronico porque recibimos una solicitud de restablecimiento de contraseña para su cuenta de Synapse."))
             ->action(Lang::get('Cambiar contraseña'), url(config('app.url') . route('password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
-            ->line(Lang::get('Si tu no realizaste la solicitud de cambio de contraseña, solo ignora este mensaje. '))
-            ->line(Lang::get('Este enlace solo es válido dentro de los proximos :count minutos.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
-            ->salutation('Synapse');
+            ->line(new HtmlString("Su correo afiliado de <strong style='color:#000000;'>Synapse</strong> es: <p style='color:#FF2B44;'>" .$notifiable->getEmailForPasswordReset(). "</p>."))
+            ->line(Lang::get('Este enlace de restablecimiento de contraseña caducará en :count minutos.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
+            ->line(Lang::get('Si tu no realizaste la solicitud de cambio de contraseña, solo ignore este mensaje. '))
+            ->line(Lang::get('Si no solicitó un restablecimiento de contraseña, no se requiere ninguna otra accion.'))
+            ->salutation(new HtmlString("<strong style='color:#000000;'>Saludos,</br> Equipo de Synapse CRM</strong>"));
     }
 }
