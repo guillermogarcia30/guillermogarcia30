342dc025a8e3eecbc7b6edf4871257efefbee1c8
Guillermo García <guillermo.garcia@betaboxtechnologies.com>
2022-06-07 14:10:26 -0400

    colocar el email de usuario en negrita

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -5,7 +5,7 @@ namespace App\Notifications;
 use \Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\Lang;
- 
+use Illuminate\Support\HtmlString;
 class ResetPassword extends ResetPasswordNotification
 {
  
@@ -18,12 +18,12 @@ class ResetPassword extends ResetPasswordNotification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->greeting('Hola! '.$notifiable->name)
+            ->greeting('Hola '.$notifiable->name)
             ->subject(Lang::get('Solicitud de restablecimiento de contraseña'))
-            ->line(Lang::get('Hola, se solicitó un restablecimiento de contraseña para tu cuenta ' . $notifiable->getEmailForPasswordReset() . ', haz clic en el botón que aparece a continuación para cambiar tu contraseña.'))
+            ->line(new HtmlString("Se solicitó un restablecimiento de contraseña para tu cuenta <strong style='color:#000000;'>" .$notifiable->getEmailForPasswordReset(). "</strong>, haz clic en el botón que aparece a continuación para cambiar tu contraseña."))
             ->action(Lang::get('Cambiar contraseña'), url(config('app.url') . route('password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
             ->line(Lang::get('Si tu no realizaste la solicitud de cambio de contraseña, solo ignora este mensaje. '))
             ->line(Lang::get('Este enlace solo es válido dentro de los proximos :count minutos.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
-            ->salutation('Saludos');
+            ->salutation('Synapse');
     }
 }
