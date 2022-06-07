3ed5c1e3d8b238c7d0c3d13597b0749eda3b93cf
Guillermo García <guillermo.garcia@betaboxtechnologies.com>
2022-06-07 08:25:21 -0400

    colocar logo de Synapse en la plantilla de correo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,8 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-{{ $slot }}
+<img src="https://auth.synapse-crm.com/assets/synapse-logo.png" style="width:50%;" alt="Synapse Logo">
+{{-- $slot --}}
 @endif
 </a>
 </td>
