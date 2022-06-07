435a14fcb7e871cc3ce03107d2fb7acb1c458e09
Guillermo García <guillermo.garcia@betaboxtechnologies.com>
2022-06-07 14:42:10 -0400

    cambiar el nombre en el email

--- config/mail.php
+++ config/mail.php
@@ -92,7 +92,8 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        //'name' => env('MAIL_FROM_NAME', 'Synapse'),
+        'name' => 'Synapse',
     ],
 
     /*
