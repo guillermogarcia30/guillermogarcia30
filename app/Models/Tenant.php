6e7e35a75714104fc052b1d62bfaca5efa752979
Guillermo García <guillermo.garcia@betaboxtechnologies.com>
2022-06-10 15:56:21 -0400

    renombrando tabla tenants mal escrita

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tenant extends Model
 {
     use HasFactory;
-    protected $table = 'tentants';
+    protected $table = 'tenants';
     protected $fillable = [
         'domain',
         'name',
